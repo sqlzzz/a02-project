592ab4691a9ace9699582067d210d89ab2fbb48a
Todd Opfermann <topfermann@live.com>
2018-10-29 00:32:36 -0400

    Commit

--- codeFolder/python-docs-samples-master/appengine/standard_python37/building-an-app/building-an-app-1/main.py
+++ codeFolder/python-docs-samples-master/appengine/standard_python37/building-an-app/building-an-app-1/main.py
@@ -26,7 +26,7 @@ def root():
     # This will be replaced with real information in later steps.
     dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
                    datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
+                   datetime.datetime(2018, 1, 4, 11, 0, 0),
                    ]
 
     return render_template('index.html', times=dummy_times)
